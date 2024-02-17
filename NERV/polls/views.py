d217ecd9e37b35df98202e337b13ee273b23e933
green760834 <ezizulanov@gmail.com>
2024-02-17 19:10:43 +0700

    commit #2

--- NERV/polls/views.py
+++ NERV/polls/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from .models import Question
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 
 def index(request):
@@ -9,7 +9,8 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("просмотр %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     resource = "результат вопроса номер %s"
